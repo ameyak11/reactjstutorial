4f3b969105ea6c1376a5a660e701df630334c474
ameyak11 <kulkarni.ameya11@gmail.com>
2020-09-17 17:14:08 -0500

    added new component

---
+++ react/confusion/src/components/MenuComponent.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Media } from "react";
+
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: [
+        {
+          id: 0,
+          name: "dish1",
+        },
+      ],
+    };
+  }
+
+  render() {
+    const menu = this.state.dishes.map((dish) => {
+      return (
+        <div key={dish.id} className="col-12 mt-5">
+          <Media tag="li">
+            <Media left middle>
+              <h4>{dish.name}</h4>
+            </Media>
+          </Media>
+        </div>
+      );
+    });
+    return (
+      <div className="container">
+        <div className="row">
+          <Media list>{menu}</Media>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Menu;
